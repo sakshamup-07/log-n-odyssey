0b06b61624b4ee532268e9418b0f3eb24c3475f5
Saksham Upadhyay <154794993+sakshamup-07@users.noreply.github.com>
2026-09-11 13:07:29 +0530

    Time: 7 ms (71.35%) | Memory: 44.9 MB (47.97%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,22 +1,25 @@
+static const int speedup = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin() , g.end());
-        sort(s.begin() , s.end());
-        if(s.size()==0) return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        if (s.size() == 0)
+            return 0;
         int p1 = 0;
         int s1 = 0;
-        int cnt =0;
-        while(p1<g.size() && s1<s.size())
-        {
-        if(min(g[p1] , s[s1])==g[p1])
-        {
-            cnt++;
-            p1++;
-            s1++;
-        }
-        else s1++;
-
+        int cnt = 0;
+        while (p1 < g.size() && s1 < s.size()) {
+            if (min(g[p1], s[s1]) == g[p1]) {
+                cnt++;
+                p1++;
+                s1++;
+            } else
+                s1++;
         }
         return cnt;
     }
